f154f1879de6308acdaf94936d8fc359f6176754
harispericorn <hari.k@spericorn.com>
2024-06-14 14:48:29 +0530

    intilaized

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import WhitelistForm from "./Components/sendToAptos.js";
+import { useState } from "react";
+import { ethers } from "ethers";
 
 function App() {
+  const [connected, setConnected] = useState(false);
+  const [walletAddress, setWalletAddress] = useState("");
+  const [signer, setSigner] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [chainId, setChainId] = useState(null);
+
+  async function connectWallet() {
+    if (!connected) {
+      // Connect the wallet using ethers.js
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+      let chainId = (await provider.getNetwork()).chainId;
+      console.log(parseInt(chainId, 10));
+      setChainId(parseInt(chainId, 10));
+      setSigner(signer);
+      setProvider(provider);
+      const _walletAddress = await signer.getAddress();
+      setConnected(true);
+      setWalletAddress(_walletAddress);
+    } else {
+      // Disconnect the wallet
+      window.ethereum.selectedAddress = null;
+      setConnected(false);
+      setWalletAddress("");
+    }
+  }
+
   return (
     <div className="App">
+      <div className="app">
+        <div className="main">
+          <div className="content">
+            <button className="btn" onClick={connectWallet}>
+              {connected ? "Disconnect Wallet" : "Connect Wallet"}
+            </button>
+            <h3>Address</h3>
+            <h4 className="wal-add">{walletAddress}</h4>
+          </div>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test project</h1>
+        <WhitelistForm
+          data={connected}
+          signer={signer}
+          provider={provider}
+          chainId={chainId}
+        />
       </header>
     </div>
   );
---
+++ src/Components/sendToAptos.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import { abi, contractAddresses } from "../Constants";
+
+const WhitelistForm = ({ data, signer, provider, chainId }) => {
+  // State to hold form values
+  const [formValues, setFormValues] = useState({
+    _token: "",
+    _toAddress: "",
+    _amountLD: "",
+    refundAddress: "",
+    zroPaymentAddress: "",
+    _adapterParams: "",
+  });
+
+  const [transactionHash, setTransactionHash] = useState(null);
+
+  // Handle input change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!signer) {
+      alert("No signer found");
+      return;
+    }
+    console.log("CHAIN", chainId);
+    const contractAddress =
+      chainId in contractAddresses ? contractAddresses[chainId][0] : null;
+    if (!contractAddress) {
+      alert("Contract is not in this network");
+      return;
+    }
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    const { refundAddress, zroPaymentAddress } = formValues;
+    try {
+      const transaction = await contract.sendToAptos(
+        formValues._token,
+        formValues._toAddress,
+        formValues._amountLD,
+        {
+          refundAddress,
+          zroPaymentAddress,
+        },
+        formValues._adapterParams
+      );
+      setTransactionHash(transaction.hash);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div>
+      {data ? (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="_token">_token:</label>
+            <input
+              type="text"
+              id="_token"
+              name="_token"
+              value={formValues._token}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="_toAddress">_toAddress:</label>
+            <input
+              type="text"
+              id="_toAddress"
+              name="_toAddress"
+              value={formValues._toAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="_amountLD">_amountLD:</label>
+            <input
+              type="text"
+              id="_amountLD"
+              name="_amountLD"
+              value={formValues._amountLD}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="refundAddress">refundAddress:</label>
+            <input
+              type="text"
+              id="refundAddress"
+              name="refundAddress"
+              value={formValues.refundAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="zroPaymentAddress">zroPaymentAddress:</label>
+            <input
+              type="text"
+              id="zroPaymentAddress"
+              name="zroPaymentAddress"
+              value={formValues.zroPaymentAddress}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="_adapterParams">_adapterParams:</label>
+            <input
+              type="text"
+              id="_adapterParams"
+              name="_adapterParams"
+              value={formValues._adapterParams}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div> Pls Connect Wallet </div>
+      )}
+      {transactionHash ? (
+        <p>Transaction Hash: {transactionHash}</p>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default WhitelistForm;
